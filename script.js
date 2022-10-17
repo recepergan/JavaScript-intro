63a5ec58b6e16e366758d701abbdc3668566c51e
recepergan <68998200+recepergan@users.noreply.github.com>
2022-10-17 11:47:37 +0300

    JavaScript-Değişkenkenler-4

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-alert("Merhaba");
-console.log(1234);
-console.log([1,2,3]);
-console.error("hata oluştı");;
-console.warn("bir uyarı oluşrıt");
+// alert("Merhaba");
+// console.log(1234);
+// console.log([1,2,3]);
+// console.error("hata oluştı");;
+// console.warn("bir uyarı oluşrıt");
+
+//Değişkenler
+// var age;
+// console.log(age);
+age = 20;
+console.log(age);
+
+var fullname='Recep Ergan';
+console.log(fullname);
+
+var fullname='kare Ergan';
+console.log(fullname);
+
+//Değişken Tanımlama
+//Sayısal ifade ile başlayamaz
+var yas1;
+var $yas3;
+
+//Komut isimleriyle tanımlama yapılamaz
+//Birden fazla kelime
+
+var ad_soyad = "Recep Ergan";
+var adSoyad = "Recep Ergan";
+
+//case sensitive
+
+var firstName = "rede";
+var FİrstName = "fsd";
+
+console.log(firstName);
+console.log(FİrstName);
+
+//var, let, const
+let city = "Kocaeli";
+console.log(city);
+
+const email = "acca@gmail.com";
+console.log(email);
+
+email = "dscd@gmail.com";
+console.log(email);
+
+    
+
+
+
+    
