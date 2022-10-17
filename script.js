024f2a4d2bbd0ca78722ce82f79e259fbe287b67
recepergan <68998200+recepergan@users.noreply.github.com>
2022-10-17 14:31:01 +0300

    JavaScript-DateTİme-10

--- script.js
+++ script.js
@@ -1,52 +1,23 @@
-//operatörler
+//Date object
 
-let val;
-const a = 10;
-const b = 5;
-let c = 3;
+let d = new Date();
+let birthday = new Date(1990,1,5);
 
+//Set Methods
+d.setFullYear(2020);
+d.getDay();
+d.setHours(10);
 
-//1- Aritmetik O.
-val = a+ b;
-val = a - b;
-val = a * b;
-val = a / b;
-val = a%b;
-// val = c++;
-// val = ++c;
-val = --c;
-val = c--;
-console.log(val);
+//Get Methods
 
-//2-Atama operatörleri
+console.log(d.getDate());
+console.log(d.getDay());
+console.log(d.getFullYear());
+console.log(d.getMonth());
+console.log(d.getHours());
+console.log(d);
+console.log(typeof d);
 
-val = a;
-console.log(val);
-val +=a; //val = val + a;
-console.log(val);
-val -=a; //val = val - a;
-
-console.log('-------');
-//3-Karsılaştırma operatörleri
-
-val = a==b
-val = b==c
-val = b===c; //değer kontrolü & type
-val = 5 ==='5';
-val = a!=b;
-val = a > b
-val = b > a
-
-//4- mantıksal operatörler
-
-//&&(and)
-
-val = (a>b) && (a>c)
-//||or)
-// ! (not)
+console.log(d.getFullYear()-birthday.getFullYear());
 
 
-
-
-console.log(val);
-console.log(typeof val);
